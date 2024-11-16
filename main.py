b96808287e1f4fb52b27abfb206da345735c9ac7
Apisit <amornchanchaigul@gmaul.com>
2024-11-16 15:06:36 +0700

    refactor code

--- main.py
+++ main.py
@@ -1,54 +1,11 @@
-import uuid
+from fastapi import FastAPI
 
-import bcrypt
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-from sqlalchemy import create_engine, Column, TEXT, VARCHAR, LargeBinary
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from database import engine
+from model.base import Base
+from routes import auth
 
 app = FastAPI()
-DATABASE_URL = 'postgresql://postgres:mysecretpassword@localhost:5432/fluttermusicapp'
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-db = SessionLocal()
 
-
-class UserCreate(BaseModel):
-    name: str
-    email: str
-    password: str
-
-
-Base = declarative_base()
-
-
-class User(Base):
-    __tablename__ = 'users'
-
-    id = Column(TEXT, primary_key=True)
-    name = Column(VARCHAR(100))
-    email = Column(VARCHAR(100))
-    password = Column(LargeBinary)
-
-
-@app.post('/signup')
-def signup_user(user: UserCreate):
-    # check if the user already exists in db
-    user_db = db.query(User).filter(User.email == user.email).first()
-
-    if user_db:
-        raise HTTPException(400, 'User with the same email already exists!')
-
-    hashed_pw = bcrypt.hashpw(user.password.encode(), bcrypt.gensalt())
-    user_db = User(id=str(uuid.uuid4()), email=user.email, password=hashed_pw, name=user.name)
-
-    # add the user to the db
-    db.add(user_db)
-    db.commit()
-    db.refresh(user_db)
-
-    return user_db
-
+app.include_router(auth.roter, prefix='/auth')
 
 Base.metadata.create_all(engine)
