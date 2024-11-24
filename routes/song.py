d136866c0f83866d56ac70caa31a00252d41c5e5
Apisit <amornchanchaigul@gmaul.com>
2024-11-24 16:44:33 +0700

    implement cloudinary

--- routes/song.py
+++ routes/song.py
@@ -18,14 +18,19 @@ cloudinary.config(
 )
 
 
-@router.post("/upload")
-def upload_song(song: UploadFile = File(...), thumbnail: UploadFile = File(...), artist: str = Form(...),
+@router.post('/upload', status_code=201)
+def upload_song(song: UploadFile = File(...),
+                thumbnail: UploadFile = File(...),
+                artist: str = Form(...),
                 song_name: str = Form(...),
-                hex_code: str = Form(...), db: Session = Depends(get_db), auth_dict=Depends(auth_middleware)):
+                hex_code: str = Form(...),
+                db: Session = Depends(get_db),
+                auth_dict=Depends(auth_middleware)):
     song_id = str(uuid.uuid4())
+    print(song_id)
     song_res = cloudinary.uploader.upload(song.file, resource_type='auto', folder=f'songs/{song_id}')
-    print(song_res)
+    print(song_res['url'])
     thumbnail_res = cloudinary.uploader.upload(thumbnail.file, resource_type='image', folder=f'songs/{song_id}')
-    print(thumbnail_res)
+    print(thumbnail_res['url'])
     # store data in db
     return 'ok'
