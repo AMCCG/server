727e71daaeea035a2a0333adaa45890e328567af
Apisit <amornchanchaigul@gmaul.com>
2024-11-24 15:50:23 +0700

    add cloudinary

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from fastapi import FastAPI
 
 from database import engine
 from model.base import Base
-from routes import auth
+from routes import auth, song
 
 app = FastAPI()
 
 app.include_router(auth.router, prefix='/auth')
+app.include_router(song.router, prefix='/song')
 
 Base.metadata.create_all(engine)
