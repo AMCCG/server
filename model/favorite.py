b4ffa69c750e1f43a9f6b137cefdb8883a102ba4
Apisit <amornchanchaigul@gmaul.com>
2024-12-12 22:11:26 +0700

    favorite part 1

---
+++ model/favorite.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Text, Column, ForeignKey
+from sqlalchemy.orm import relationship
+
+from model.base import Base
+
+
+class Favorite(Base):
+    __tablename__ = 'favorite'
+
+    id = Column(Text, primary_key=True)
+    song_id = Column(Text, ForeignKey('songs.id'))
+    user_id = Column(Text, ForeignKey('users.id'))
+
+    song = relationship('Song')
--- routes/song.py
+++ routes/song.py
@@ -1,14 +1,16 @@
 import uuid
 
 from fastapi import APIRouter, UploadFile, File, Form, Depends
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, joinedload
 
 from database import get_db
 from middleware.auth_middleware import auth_middleware
 import cloudinary
 import cloudinary.uploader
 
+from model.favorite import Favorite
 from model.song import Song
+from pydantic_schemas.favorite_song import FavoriteSong
 
 router = APIRouter()
 
@@ -53,3 +55,26 @@ def upload_song(song: UploadFile = File(...),
 def list_song(db: Session = Depends(get_db), auth_details=Depends(auth_middleware)):
     songs = db.query(Song).all()
     return songs
+
+
+@router.post('/favorite')
+def favorite_song(song: FavoriteSong, db: Session = Depends(get_db), auth_details=Depends(auth_middleware)):
+    user_id: str = auth_details['uid']
+    fav_song = db.query(Favorite).filter(Favorite.song_id == song.song_id
+                                         , Favorite.user_id == user_id).first()
+    if fav_song:
+        db.delete(fav_song)
+        db.commit()
+        return {'message': False}
+    else:
+        new_song = Favorite(id=str(uuid.uuid4()), song_id=song.song_id, user_id=user_id)
+        db.add(new_song)
+        db.commit()
+        return {'message': True}
+
+
+@router.get("/list/favorite")
+def list_song(db: Session = Depends(get_db), auth_details=Depends(auth_middleware)):
+    user_id: str = auth_details['uid']
+    fav_songs = db.query(Favorite).filter(Favorite.user_id == user_id).options(joinedload(Favorite.song)).all()
+    return fav_songs
